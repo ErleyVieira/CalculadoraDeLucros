7e3ab02a349b23b993e163b8f005fd1285265a6f
Erley Jonhson <dantaserley@gmail.com>
2021-12-04 21:49:58 -0300

    Projeto em 50%

---
+++ CalculadoraDeLucros/Produtos.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalculadoraDeLucros
+{
+    internal class Produtos
+    {
+        
+
+        public double Pedido()
+        {
+            Console.WriteLine("Qual o valor da venda ?");
+            double valorVenda = double.Parse(Console.ReadLine());
+            double valorDescontado = 0;
+            Console.WriteLine("[1]Ifood" +
+                                "[2]Ifood com pagamento na entrega" +
+                                "[3]Uber entrega própria" +
+                                "[4]Uber entrega parceira" +
+                                "[5]Rappi" +
+                                "Qual a plataforma dessa venda ?))");
+            int opcao = int.Parse(Console.ReadLine());
+            if (opcao == 1)
+                
+                valorDescontado = valorVenda - valorVenda * 0.155;
+            
+            else if (opcao == 2)
+                
+                valorDescontado = valorVenda - valorVenda * 0.12;
+            
+            else if (opcao == 3)
+                
+                valorDescontado = valorVenda - valorVenda * 0.17;
+
+            else if (opcao == 4)
+
+                valorDescontado = valorVenda - valorVenda * 0.3;
+
+            else if (opcao == 5)
+
+                valorDescontado = valorVenda - valorVenda * 0.1;
+
+            return double.Parse(Console.WriteLine($"O valor da venda com descontos foi de {valorDescontado}"));
+        }
+    }
+}
---
+++ CalculadoraDeLucros/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace CalculadoraDeLucros
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Produtos p = new Produtos();
+
+            double venda = p.PedidoPedido();
+
+        }
+    }
+}
